cda7f0ede512955a3d9573c7cb7d8d6f7a057bf6
jdichh <jpsdichoso21@gmail.com>
2023-09-21 13:05:50 +0800

    removed redundant styling, changed header styling

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -3,6 +3,9 @@
 import Image from "next/image";
 import React from "react";
 import { motion } from "framer-motion";
+import { Inter } from "next/font/google";
+
+const inter = Inter ({ subsets: ["latin"] });
 
 export default function About() {
   return (
@@ -13,7 +16,7 @@ export default function About() {
           initial={{ opacity: 0, scale: 0 }}
           animate={{ opacity: 1, scale: 1 }}
         >
-          <h1 className="text-5xl text-[#6E99C4]">Jason Dichoso</h1>
+          <h1 className={`${inter.className} font-medium text-4xl text-[#6E99C4] sm:text-5xl`}>Jason Dichoso</h1>
           <h2 className="text-base">Aspiring Frontend Developer</h2>
         </motion.div>
         <div className="flex">
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,19 +7,19 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="z-[99] relative">
+    <header className="z-[99] relative ">
       <motion.div
-        className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none bg-[#E9E9E9] bg-opacity-95 sm:top-6 sm:h-[3.5rem] sm:w-[18rem] sm:rounded-md"
+        className="fixed left-1/2 border-b border-[#6E99C4] bg-[#131217] bg-opacity-95 top-0 h-[3.5rem] w-full rounded-sm"
         initial={{ y: -50, opacity: 0, x: "-50%" }}
         animate={{ y: 0, opacity: 1, x: "-50%" }}
       />
-      <nav className="fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
-        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] sm:w-[initial] sm:flex-nowrap sm:gap-6">
+      <nav className="fixed left-1/2 -translate-x-1/2 top-0">
+        <ul className="flex w-[22rem] justify-center gap-6">
             {links.map(link => (
-                <motion.li key={link.hash} className="text-[#131217] h-3/4 flex items-center justify-center text-base"  
+                <motion.li key={link.hash} className="flex text-[#E9E9E9]"  
                 initial={{ y: -50, opacity: 0 }}
                 animate={{ y: 0, opacity: 1 }}>
-                    <Link href={link.hash} className="flex items-center justify-center p-3 w-full hover:font-medium">{link.name}</Link>
+                    <Link href={link.hash} className="p-3 transition duration-150 ease-in-out text-lg hover:text-[#6E99C4]">{link.name}</Link>
                 </motion.li>
             ))}
         </ul>
