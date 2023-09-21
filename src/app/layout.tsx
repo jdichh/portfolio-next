45bec1077e43331596c5f4084061f506388687c0
jdichh <jpsdichoso21@gmail.com>
2023-09-21 11:21:59 +0800

    initial header styling and anims

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,29 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import Header from "@/components/header";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Jason Dichoso | Portfolio',
-  description: 'Jason Dichoso is a frontend web developer.',
-}
+  title: "Jason Dichoso | Portfolio",
+  description: "Jason Dichoso is a frontend web developer.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${inter.className} bg-[#1A1A1A] text-[#DADADA] relative h-[5000px]`}
+      >
+        <Header />
+        {children}
+        <h1>test</h1>
+      </body>
     </html>
-  )
+  );
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React from "react";
+import { motion } from "framer-motion";
+
+export default function Header() {
+  return (
+    <header className="z-[99] relative">
+      <motion.div
+        className="fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-white/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full"
+        initial={{ y: -50, opacity: 0, x: "-50%" }}
+        animate={{ y: 0, opacity: 1, x: "-50%" }}
+      ></motion.div>
+    </header>
+  );
+}
