0a380351180dd943290a39e3a400bc5db8e95319
jdichh <jpsdichoso21@gmail.com>
2023-09-22 11:48:16 +0800

    increased section heading & header font weights

--- src/components/section-heading.tsx
+++ src/components/section-heading.tsx
@@ -6,7 +6,7 @@ type SectionHeadingProps = {
 
 export default function SectionHeading({children}: SectionHeadingProps) {
   return (
-    <h3 className="text-lg md:text-xl text-center mb-3 mt-12 xl:mt-0">
+    <h3 className="text-lg md:text-xl text-center mb-3 mt-12 xl:mt-0 font-medium">
       {children}
     </h3>
   );
