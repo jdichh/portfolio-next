76d897e4c13acec7790c917a2565f173e1dfe2d7
jdichh <jpsdichoso21@gmail.com>
2023-09-22 11:46:57 +0800

    increased proj title font weight, changed colors

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -35,14 +35,14 @@ export default function Project({
     >
       <section className="group overflow-hidden bg-[#1D1C23] rounded-sm relative hover:bg-[#23222B] transition ease-in-out duration-150 outline outline-1 outline-[#6E99C4]">
         <div className="flex flex-col py-6 pb-7 px-4 sm:px-8 md:max-w-[50%] xl:max-w-[55%] md:min-h-[16rem]">
-          <h3 className="text-xl text-center sm:text-2xl sm:text-left text-[#6E99C4]">
+          <h3 className="text-xl text-center sm:text-2xl sm:text-left text-[#E9E9E9] font-bold">
             {name}
           </h3>
-          <ul className="flex flex-wrap my-2 gap-2 text-sm justify-center sm:justify-start font-bold">
+          <ul className="flex flex-wrap my-2 gap-2 text-sm justify-center sm:justify-start font-medium">
             {tools.map((tool, index) => (
               <li
                 key={index}
-                className="bg-[#E9E9E9] px-2 py-0 uppercase tracking-wider text-[#1F1E26] rounded-sm"
+                className="bg-[#6E99C4] px-2 py-0 uppercase tracking-wider text-[#1F1E26] rounded-sm"
               >
                 {tool}
               </li>
