1ce940ead9426373b0163c5247c408a497844a79
jdichh <jpsdichoso21@gmail.com>
2023-09-22 11:31:56 +0800

    added footer anims and initial pwa config

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,15 @@
+"use client";
+
 import React from "react";
+import { motion } from "framer-motion";
 
 export default function Footer() {
   return (
-    <footer className="my-6 border-t-2 w-full lg:w-10/12 xl:max-w-[1395px] border-[#6E99C4]">
+    <motion.footer
+      className="my-6 border-t-2 w-full lg:w-10/12 xl:max-w-[1395px] border-[#6E99C4]"
+      initial={{ y: 100, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+    >
       <div className="flex flex-col sm:w-10/12 mx-auto text-sm sm:text-base px-4">
         <blockquote className="italic text-center my-6">
           “That has been one of my mantras - focus and simplicity. Simple can be
@@ -10,6 +17,6 @@ export default function Footer() {
         </blockquote>
         <b className="text-right">- Plato</b>
       </div>
-    </footer>
+    </motion.footer>
   );
 }
