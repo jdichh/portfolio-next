61322b666962feb05a94a14d3203fecfcf65df37
jdichh <jpsdichoso21@gmail.com>
2023-09-24 07:47:03 +0800

    removing portfolio from projects

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -239,15 +239,6 @@ export const designTools = [
 
 export const projects = [
   {
-    name: "This Portfolio",
-    description:
-      "What you're looking at right now. This is the new version of my portfolio that I designed myself. The initial version was made with HTML, SCSS and JS only, but I figured that I should learn Next.js by recreating it.",
-    tools: ["Recommended on PC", "PWA Optimized", "Next.js 13", "TypeScript", "Framer Motion"],
-    imageUrl: portfolio,
-    githubLink: "https://github.com/jdichh/portfolio-next",
-    oldVersion: "https://jdichh.netlify.app"
-  },
-  {
     name: "Virtual Car Showroom",
     description:
       "A 3D virtual car showroom mainly inspired by Gran Turismo Sport. I also tried replicating some parts of the UI.",
