c9fec70691de98b96b922f152cbdf61ec5c139ba
jdichh <jpsdichoso21@gmail.com>
2023-09-24 16:20:12 +0800

    added framer motion anim

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,20 +1,26 @@
+"use client"
+
 import React from "react";
 import SectionHeading from "./section-heading";
 import { projects } from "@/lib/data";
 import Project from "./project";
+import { motion } from "framer-motion";
 
 export default function Projects() {
   return (
-    <section
+    <motion.section
       className="px-6 w-full xl:w-10/12 leading-8 scroll-mt-20"
       id="projects"
-    >
-      <SectionHeading>Personal Projects</SectionHeading>
-        {projects.map((project, index) => (
-          <React.Fragment key={index}>
-            <Project {...project} />
-          </React.Fragment>
-        ))}
-    </section>
+      initial={{ y: 100, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      >
+        <SectionHeading>Personal Projects</SectionHeading>
+
+      {projects.map((project, index) => (
+        <React.Fragment key={index}>
+          <Project {...project} />
+        </React.Fragment>
+      ))}
+    </motion.section>
   );
 }
