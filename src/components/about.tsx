dc0a97b9f354acb5295e06de8e2f71ef190e7da7
jdichh <jpsdichoso21@gmail.com>
2023-09-26 01:00:25 +0800

    small revisions, added <p> tag

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -14,12 +14,13 @@ export default function About() {
       <SectionHeading>About Me</SectionHeading>
       <p className="mb-6">
         I am a fresh IT graduate based in the Philippines, looking to enter the
-        IT industry as a{" "}
+        industry as a{" "}
         <span className="text-[#6E99C4] font-medium">
           frontend web developer
         </span>
-        . Since I started my self-learning journey, I've discovered that I
-        really <span className="text-[#6E99C4] font-medium">love learning</span>{" "}
+        . Since I started my self-learning journey in early 2023, I've
+        discovered that I really{" "}
+        <span className="text-[#6E99C4] font-medium">love learning</span>{" "}
         various web technologies, especially front-end tech. And when I get to
         scratch the surface of what a piece of software can be capable of, I get{" "}
         <span className="text-[#6E99C4] font-medium">excited</span> to see what
@@ -32,6 +33,10 @@ export default function About() {
           good-looking and user friendly websites
         </span>
         .
+      </p>
+      <p className="mb-6">
+        Currently, I'm trying to learn more about testing with Jest and
+        React-Testing-Library.
       </p>
     </motion.section>
   );
