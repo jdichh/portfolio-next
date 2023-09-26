7da7ab8a6b0c481fc12bd233392e4b7e3237fb6b
jdichh <jpsdichoso21@gmail.com>
2023-09-27 00:59:57 +0800

    revised about me text

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -7,32 +7,13 @@ import SectionHeading from "./section-heading";
 export default function About() {
   return (
     <motion.section
-      className="px-6 w-full lg:w-8/12 xl:w-6/12 leading-relaxed text-left my-20 scroll-mt-[2rem] lg:scroll-mt-20"
+      className="px-6 w-full lg:w-8/12 xl:w-6/12 leading-relaxed text-center my-20 scroll-mt-[2rem] lg:scroll-mt-20"
       initial={{ y: 100, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
     >
       <SectionHeading>About Me</SectionHeading>
       <p className="mb-6">
-        I am a fresh IT graduate based in the Philippines, determined to enter the
-        industry as a{" "}
-        <span className="text-[#6E99C4] font-medium">
-          frontend web developer
-        </span>
-        . Since I started my self-learning journey in early 2023, I've
-        discovered that I really{" "}
-        <span className="text-[#6E99C4] font-medium">love learning</span>{" "}
-        various web technologies, especially front-end tech. And when I get to
-        scratch the surface of what a piece of software can be capable of, I get{" "}
-        <span className="text-[#6E99C4] font-medium">excited</span> to see what
-        else it can do.
-      </p>
-      <p className="mb-6">
-        One of my many goals is to help individuals and/or businesses present
-        themselves in the digital world by creating{" "}
-        <span className="text-[#6E99C4] font-medium">
-          good-looking and user friendly websites
-        </span>
-        .
+        I aim to <span className="text-[#6E99C4] font-medium">user-friendly and impressive-looking websites</span> here in the Philippines.
       </p>
     </motion.section>
   );
