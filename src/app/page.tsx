e92680de20eee3987e591438fe1ee31952b03e65
jdichh <jpsdichoso21@gmail.com>
2023-09-28 01:48:36 +0800

    fixed import issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
-import About from "@/components/About";
-import Hero from "@/components/Hero";
-import Projects from "@/components/Projects";
+import About from "@/components/about";
+import Hero from "@/components/hero";
+import Projects from "@/components/projects";
 import SectionDivider from "@/components/SectionDivider";
-import Tools from "@/components/Tools";
-import Footer from "@/components/Footer"
+import Tools from "@/components/tools";
+import Footer from "@/components/footer";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center">
       <Hero />
-      <SectionDivider/>
+      <SectionDivider />
       <About />
       <Tools />
       <Projects />
       <Footer />
     </main>
-  )
+  );
 }
