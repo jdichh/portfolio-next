2639c024015b3b514430f5519d99cbdfb42c5758
jdichh <jpsdichoso21@gmail.com>
2023-09-28 09:02:37 +0800

    narrower widths for about text container

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -7,7 +7,7 @@ import SectionHeading from "./SectionHeading";
 export default function About() {
   return (
     <motion.section
-      className="px-3 w-full lg:w-8/12 xl:w-6/12 leading-relaxed text-center my-20 scroll-mt-[2rem] lg:scroll-mt-20"
+      className="px-3 w-9/12 md:w-6/12 xl:w-5/12 leading-relaxed text-left md:text-center my-20 scroll-mt-[2rem] lg:scroll-mt-20"
       initial={{ y: 100, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{ delay: 0.5, ease: [0.22, 1, 0.36, 1] }}
@@ -19,9 +19,10 @@ export default function About() {
           user-friendly and impressive-looking websites
         </span>{" "}
         here in the Philippines.
-        <br />
+      </p>
+      <p className="mb-6">
         I'm also a recent IT graduate of Colegio de San Juan De Letran-Calamba,
-        <br /> and{" "}
+        and{" "}
         <span className="text-[#6E99C4] font-medium">
           looking to get my foot in the door in the tech industry
         </span>
