542307cc316478f1e86ecd2b7f811c829442a028
jdichh <jpsdichoso21@gmail.com>
2023-10-02 00:36:36 +0800

    fixed header border-b fouc

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,14 +7,14 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="z-[99] relative w-full font-medium">
-      <motion.div
-        className="fixed left-1/2 border-b-2 top-0 h-16 w-full theme-switch"
-        initial={{ opacity: 0, x: "-50%" }}
-        animate={{ opacity: 1, x: "-50%" }}
-        transition={{ delay: 0.15, ease: [0.22, 1, 0.36, 1] }}
-      />
-      <nav className="fixed left-1/2 -translate-x-[50%] top-0 flex items-center">
+    <motion.header
+      className="z-[99] relative w-full font-medium"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 0.15, ease: [0.22, 1, 0.36, 1] }}
+    >
+      <div className="fixed left-1/2 -translate-x-[50%] border-b-2 top-0 h-16 w-full theme-switch" />
+      <nav className="fixed left-1/2 -translate-x-[50%] top-0 flex items-center ">
         <motion.ul
           className="flex flex-row justify-center gap-6 sm:gap-10"
           initial={{ opacity: 0 }}
@@ -25,7 +25,7 @@ export default function Header() {
             <li key={link.hash} className="flex text-[#236660]">
               <Link
                 href={link.hash}
-                className="p-4 text-lg text-light-theme-text dark:text-dark-theme-text hover:text-[#236660] dark:hover:text-[#236660] rounded-sm"
+                className="p-4 text-lg theme-switch hover:text-[#236660] dark:hover:text-[#236660] rounded-sm"
               >
                 {link.name}
               </Link>
@@ -33,6 +33,6 @@ export default function Header() {
           ))}
         </motion.ul>
       </nav>
-    </header>
+    </motion.header>
   );
 }
